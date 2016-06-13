4394adac178235a8cb6344a48a547315fc48dace
Michael 'afrolion' Perrotte <mikemimik@gmail.com>
2016-06-13 11:55:35 -0400

    updated node LTS version number

--- manifests/globals.pp
+++ manifests/globals.pp
@@ -6,6 +6,6 @@ class nodejs::globals (
 ) {
   $default_bin  = pick($bin, '/usr/local/bin:/usr/bin:/bin')
   $default_user = pick($user, 'vagrant')
-  $node_version = pick($version, '4.4.4')
+  $node_version = pick($version, '4.4.5')
   $home_dir     = pick($home, "/home/${default_user}")
 }
